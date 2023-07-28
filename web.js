93a29352415313951e77d7a100b89c6c923b8934
Aditi <127743490+Aditi-Shaw-aditishaw@users.noreply.github.com>
2023-07-29 00:12:20 +0530

    Add files via upload

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
-alert('welcome');
-let wannaBUY = document.querySelector('button');
-wannaBUY.addEventListener('click', inputSize);
+alert('welcome to ahy team web');
 
+function myFunction() {
+    alert("please fill the following field to continue shopping!!");
+    // Your other JavaScript code can go here
+    var address = prompt("enter address:");
 
-function inputSize() {
-    let size = prompt('enter size');
-    wannaBUY.textContent = 'sizes' + size;
+    if (address = null) {
+        alert("No input provided.");
+    }
+    var Size = prompt("enter size in XL L M S XS:");
+    if (Size == null) { alert("No input provided."); }
+    var productName = prompt("please carefull read and write the product name:");
+    if (productName == null) { alert("No input provided."); }
+    if (address !== null && Size !== null && productName !== null) {
+        alert("seller is processing your order!!\n order recived.....");
+    } else(alert("please enter all the values"))
 }
